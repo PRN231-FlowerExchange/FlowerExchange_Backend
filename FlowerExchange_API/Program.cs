8889d5924d9cb8d8189a25a65a227fde1d32a508
Ishha <nguyenhoanganhgoahead@gmail.com>
2024-10-04 21:40:15 +0700

    Update Program.cs

--- FlowerExchange_API/Program.cs
+++ FlowerExchange_API/Program.cs
@@ -132,11 +132,10 @@ var app = builder.Build();
 
 //initial data
 InitialiserExtensions.InitialiseDatabaseAsync(app);
-
+app.UseSwagger();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
+{ 
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Flower Exchange API V1");
