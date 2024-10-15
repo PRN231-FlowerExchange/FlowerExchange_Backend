03bd084e520099ae1f8274be3d257122558140e4
Nhat Truong <nhattruong3314@gmail.com>
2024-10-15 21:49:22 +0700

    Create GetTopPostQuery.cs

---
+++ FlowerExchange_Services/PostFlower/Queries/GetPost/GetTopPostQuery.cs
@@ -0,0 +1,12 @@
+﻿Qusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.PostFlower.Queries.GetPost
+{
+    public class GetTopPostQuery
+    {
+    }
+}
