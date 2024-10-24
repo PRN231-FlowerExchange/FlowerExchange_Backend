174027338abde141bf40e22c56619a84ad3188ce
Hoang Anh Nguyen <nguyenhoanganhgoahead@gmail.com>
2024-10-24 11:53:44 +0700

    enhance feature google login

---
+++ FlowerExchange_Services/UserIdentity/Queries/ExternalLogin/ExternalLoginCommand.cs
@@ -0,0 +1,42 @@
+﻿using Application.UserIdentity.Commands.ExternalLogin;
+using Domain.Commons.BaseRepositories;
+using Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Application.UserIdentity.Queries.ExternalLogin
+{
+    public record ExternalLoginCommand : IRequest<AuthenticationProperties>
+    {
+        [DefaultValue("Google")]
+        public string AuthenticationScheme;
+
+        
+        [Required]        
+        public string RedirectUrl;
+    }
+
+    public class ExternalLoginCommandHandler : IRequestHandler<ExternalLoginCommand, AuthenticationProperties>
+    {
+        private readonly SignInManager<User> _signInManager;
+
+
+        public ExternalLoginCommandHandler(IServiceProvider serviceProvider)
+        {
+            _signInManager = serviceProvider.GetRequiredService<SignInManager<User>>();
+        }
+
+        public async Task<AuthenticationProperties> Handle(ExternalLoginCommand request, CancellationToken cancellationToken)
+        {
+            AuthenticationProperties properties =  _signInManager.ConfigureExternalAuthenticationProperties(request.AuthenticationScheme, request.RedirectUrl);
+            return properties;
+        }
+    }
+}
