fdece9c3a3a95496799658e4e9459e28c054bcb5
TyKay264 <khaipt4working@gmail.com>
2024-10-29 16:07:01 +0700

    feat/add-signalR

--- FlowerExchange_API/Program.cs
+++ FlowerExchange_API/Program.cs
@@ -9,6 +9,7 @@ using Infrastructure.Payment;
 using Microsoft.AspNetCore.Diagnostics;
 using Newtonsoft.Json;
 using Persistence;
+using Presentation;
 using Presentation.OptionsSetup;
 using System.Reflection;
 
@@ -92,6 +93,7 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 
 // VNPAY service
 builder.Services.AddScoped<IVNPAYService, VNPAYService>();
+builder.Services.AddSignalR();
 
 // Momo service
 builder.Services.Configure<MomoOptionModel>(builder.Configuration.GetSection("MomoAPI"));
@@ -158,7 +160,15 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseRouting();
 
 //app.MapIdentityApi<User>();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHub<ChatHub>("/chatHub"); // SignalR Hub endpoint
+});
+
 
 app.Run();
